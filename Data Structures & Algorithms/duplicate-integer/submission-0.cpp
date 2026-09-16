2ad8d6714cd59ee28b9774d63ca0bfafb774df92
Illia Guzerya <53150959+Rizame@users.noreply.github.com>
2026-09-16 15:05:47 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        std::unordered_map<int, int> m;
+        for(int i = 0;i < n; i++){
+            m[nums[i]]++;
+        }
+        for(int i = 0; i < n; i++){
+            if(m[nums[i]] > 1) return true;
+        }
+        return false;
+    }
+};
